4a9c91c6cc502a2caa94ee5acc8e393da6719117
Tatsuro Muto <tomuto0221@gmail.com>
2020-07-14 08:34:25 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+// 8と4をかけた結果を出力してください
+console.log(8 * 4);
+
+// 24を4で割った結果を出力してください
+console.log(24 / 4);
+
+// 7を2で割った余りを出力してください
+console.log(7 % 2);
+
+
+const name = "にんじゃわんこ";
+const age = 14;
+
+// 「ぼくの名前は〇〇です」とコンソールに出力してください
+console.log(`ぼくの名前は ${name}です`);
+
+// 「今は〇〇歳です」と出力してください
+console.log(`今は${age}歳です`);
+
+alert('hello, world');
